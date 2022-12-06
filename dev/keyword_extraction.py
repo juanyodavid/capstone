f4dfbed63e1dc9abe825751e9bdce4bc0a24f5c0
Rad710 <rolmedro@gmail.com>
2022-12-06 23:46:37 +0800

    more accuracies

--- dev/keyword_extraction.py
+++ dev/keyword_extraction.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac15f830f832f39c70d1c9da93f0a60668410a9a34ae51dbe608f94ade184211
-size 4667
+oid sha256:d5ca1ea070cccc7ff066801ef7d8d0017e507a26eea623d1780aa7c725a10731
+size 6011
